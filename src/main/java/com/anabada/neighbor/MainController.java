c1b3ed03389ee1be8748109f481451eb1d2edd7d
우병걸 <wbg030281@gmail.com>
2023-06-20 15:48:31 +0900

    우병걸/0620/카카오지도api (아직미완..마커의 위도경도찾기)

--- src/main/java/com/anabada/neighbor/MainController.java
+++ src/main/java/com/anabada/neighbor/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.lang.model.SourceVersion;
@@ -16,15 +17,22 @@ import java.util.List;
 public class MainController {
     private final UsedService usedService;
     private final ClubService clubService;
+
     @Autowired
     public MainController(UsedService usedService, ClubService clubService) {
         this.usedService = usedService;
         this.clubService = clubService;
     }
+
     @GetMapping("/")
-    public String main(Model model){
+    public String main(Model model) {
         model.addAttribute("starList", usedService.mainList());
 //        model.addAttribute("club", clubService.mainList());
         return "index";
+    }
+
+    @GetMapping("/ex")
+    public String ex(){
+        return "chat/mapEx";
     }
 }
